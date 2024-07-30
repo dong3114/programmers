48e2eefea1563fc8dc73d4371026b22c0f50dd8c
dong3114 <scv3114@gmail.com>
2024-07-30 23:52:52 +0900

    programmers 181949

---
+++ src/main/programmers/lssons181949/Solution.java
@@ -0,0 +1,27 @@
+package main.programmers.lssons181949;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> array = new ArrayList<>();
+        String a = sc.next();
+        String str = a.toUpperCase();
+        for(int i = 0; i < a.length(); i++) {
+                String big = a.substring(i, i+1);
+                String scan = str.substring(i, i+1);
+                if(big.equals(scan)) {
+                    big = big.toLowerCase();
+                    array.add(big);
+                } else {
+                    big = big.toUpperCase();
+                    array.add(big);
+                }
+        }
+        String sum = String.join("", array);
+
+        System.out.println(sum);
+    }
+}

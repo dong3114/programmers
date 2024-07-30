8f9bb21db0179ef8e6d924d88e4e2293649257ff
dong3114 <scv3114@gmail.com>
2024-07-30 23:56:33 +0900

    programmers 181949

--- src/main/programmers/lssons181949/Solution.java
+++ src/main/programmers/lssons181949/Solution.java
@@ -10,18 +10,18 @@ public class Solution {
         String a = sc.next();
         String str = a.toUpperCase();
         for(int i = 0; i < a.length(); i++) {
-                String big = a.substring(i, i+1);
-                String scan = str.substring(i, i+1);
-                if(big.equals(scan)) {
-                    big = big.toLowerCase();
-                    array.add(big);
-                } else {
-                    big = big.toUpperCase();
-                    array.add(big);
-                }
+            //@params=big"a에서 대문자인 경우 찾기위한 변수"
+            String big = a.substring(i, i+1);
+            String scan = str.substring(i, i+1);
+            if(big.equals(scan)) {
+                big = big.toLowerCase();
+                array.add(big);
+            } else {
+                big = big.toUpperCase();
+                array.add(big);
+            }
         }
         String sum = String.join("", array);
-
         System.out.println(sum);
     }
 }

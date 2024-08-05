c4a490f138925b7b4d9a3c273e28d0c308a1c880
dong3114 <scv3114@gmail.com>
2024-08-05 23:31:11 +0900

    programmers

---
+++ src/main/programmers/lessons181941/Main.java
@@ -0,0 +1,11 @@
+package main.programmers.lessons181941;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        String[] arr = new String[]{"a", "b", "c"};
+        String answer = sl.solution(arr);
+        System.out.println(answer);
+    }
+}

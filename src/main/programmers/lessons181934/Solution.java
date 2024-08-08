09cb193569990d94caa232620bf9f118c9349fe8
dong3114 <scv3114@gmail.com>
2024-08-08 23:58:26 +0900

    programmers

--- src/main/programmers/lessons181934/Solution.java
+++ src/main/programmers/lessons181934/Solution.java
@@ -3,13 +3,13 @@ package main.programmers.lessons181934;
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
-        if(n >= m && ineq == ">" && eq == "=") {
+        if(n >= m && ineq.equals(">") && eq.equals("=")) {
             answer = 1;
-        } else if(n <= m && ineq == "<" && eq == "=") {
+        } else if(n <= m && ineq.equals("<") && eq.equals("=")) {
             answer = 1;
-        } else if(n > m && ineq == ">" && eq == "!") {
+        } else if(n > m && ineq.equals(">") && eq.equals("!")) {
             answer = 1;
-        } else if(n < m && ineq == "<" && eq == "!") {
+        } else if(n < m && ineq.equals("<") && eq.equals("!")) {
             answer = 1;
         } else {
             answer = 0;

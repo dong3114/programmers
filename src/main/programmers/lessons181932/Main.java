409a94251fa052bb3af62b1d4fdb57f329aff5cf
dong3114 <scv3114@gmail.com>
2024-08-16 01:15:49 +0900

    programmers

--- src/main/programmers/lessons181932/Main.java
+++ src/main/programmers/lessons181932/Main.java
@@ -3,6 +3,6 @@ package main.programmers.lessons181932;
 public class Main {
     public static void main(String[] args) {
         Solution sl = new Solution();
-        System.out.println(sl.solution("abc1abc"));
+        System.out.println(sl.solution(""));
     }
 }
--- src/main/programmers/lessons181932/Solution.java
+++ src/main/programmers/lessons181932/Solution.java
@@ -7,6 +7,8 @@ class Solution {
         String answer = "";
         int mode = 0;
 
+        StringBuilder str = new StringBuilder();
+
         ArrayList arr = new ArrayList();
 
         ArrayList sum = new ArrayList();
@@ -16,21 +18,27 @@ class Solution {
         }
 
         for(int i = 0; i < arr.size(); i++) {
-            if(mode == 0 && arr.get(i) != "1" && i % 2 == 0){
-                sum.add(arr.get(i));
-            } else if(mode == 0 && arr.get(i) == "1"){
+            if(mode == 0 && arr.get(i).equals('1')){
                 mode = 1;
-            } else if(mode == 1 && arr.get(i) != "1" && i % 2 == 1){
-                sum.add(arr.get(i));
-            } else if(mode == 1 && arr.get(i) == "1") {
+            } else if(mode == 1 && arr.get(i).equals('1')){
                 mode = 0;
-            } else if(arr.size() < 0) {
-                answer = "EMPTY";
-                break;
             }
-            answer = sum.toString();
+            if(mode == 0 && i % 2 == 0){
+                if(!arr.get(i).equals('1')) {
+                    sum.add(arr.get(i));
+                    str.append(arr.get(i));
+                }
+            } else if(mode == 1 && i % 2 == 1){
+                if(!arr.get(i).equals('1')) {
+                    sum.add(arr.get(i));
+                    str.append(arr.get(i));
+                }
+            }
+            answer = String.valueOf(str);
         }
-        System.out.println(mode);
+        if(sum.isEmpty()){
+            return answer = "EMPTY";
+        }
 
         return answer;
     }

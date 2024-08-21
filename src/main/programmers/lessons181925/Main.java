de94d74b189ba8192941ff89e26120eb1f26aaae
dong3114 <scv3114@gmail.com>
2024-08-22 00:48:47 +0900

    programmers

---
+++ src/main/programmers/lessons181925/Main.java
@@ -0,0 +1,9 @@
+package main.programmers.lessons181925;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        int[] numLog = {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1};
+        System.out.println(sl.solution(numLog));
+    }
+}

abfa25bbd730b9ae278030fc5e3cdc56cdd4d28d
dong3114 <scv3114@gmail.com>
2024-08-28 04:35:41 +0900

    programmers

---
+++ src/main/programmers/lessons181915/Main.java
@@ -0,0 +1,10 @@
+package main.programmers.lessons181915;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        String my_string = "cvsgiorszzzmrpaqpe";
+        int[] index_list = {16,6,5,3,12,14,11,11,17,12,7};
+        System.out.println(sl.solution(my_string, index_list));
+    }
+}
---
+++ src/main/programmers/lessons181915/Solution.java
@@ -0,0 +1,22 @@
+package main.programmers.lessons181915;
+
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+
+        ArrayList<Character> cha = new ArrayList<>();
+        for(int i = 0; i < my_string.length(); i++){
+            char str = 0;
+            str = my_string.charAt(i);
+            cha.add(str);
+        }
+        for(int i = 0; i < index_list.length; i++){
+            answer += cha.get(index_list[i]);
+        }
+
+        return answer;
+    }
+}

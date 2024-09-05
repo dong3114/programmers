fd4120bb1a5de0947a1339169b236106f2db6d8c
dong3114 <scv3114@gmail.com>
2024-09-05 20:04:26 +0900

    programmers

---
+++ src/main/programmers/lessons181907/Main.java
@@ -0,0 +1,15 @@
+package main.programmers.lessons181907;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        String my_string = "ProgrammerS123";
+        int n = 11;
+        String result = sl.solution(my_string, n);
+
+        System.out.println(result);
+    }
+
+
+
+}

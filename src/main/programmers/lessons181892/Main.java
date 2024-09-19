b53a3c4d89b7bb299076af3ab1db576669f4573f
dong3114 <scv3114@gmail.com>
2024-09-19 20:02:02 +0900

    programmers

---
+++ src/main/programmers/lessons181892/Main.java
@@ -0,0 +1,14 @@
+package main.programmers.lessons181892;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        int[] num_list = {2,1,6};
+        int n = 3;
+        String result = Arrays.toString(new ArrayList[]{sl.solution(num_list, n)});
+        System.out.println(result);
+    }
+}

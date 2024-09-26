f3c42c2b279fa101da725a36f0fa414a313bb177
dong3114 <scv3114@gmail.com>
2024-09-26 23:00:56 +0900

    programmers

---
+++ src/main/programmers/lessons181887/Main.java
@@ -0,0 +1,10 @@
+package main.programmers.lessons181887;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        int[] num_list = {4,2,6,1,7,6};
+        int result = sl.solution(num_list);
+        System.out.println(result);
+    }
+}

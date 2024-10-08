9b73fd3abfb6248331c577a3c6aa30263d1d28ab
dong3114 <scv3114@gmail.com>
2024-10-08 22:55:56 +0900

    programmers

---
+++ src/main/programmers/lessons181876/Main.java
@@ -0,0 +1,9 @@
+package main.programmers.lessons181876;
+
+public class Main {
+    public static void main(String[] args){
+        Solution sl = new Solution();
+        String myString = "aBcDeFg";
+        System.out.println(sl.solution(sl.solution(myString)));
+    }
+}
---
+++ src/main/programmers/lessons181876/Solution.java
@@ -0,0 +1,8 @@
+package main.programmers.lessons181876;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toLowerCase();
+        return answer;
+    }
+}

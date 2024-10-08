dfff47f154e03d29e17a49e3d1cc21ff5a64ac6d
dong3114 <scv3114@gmail.com>
2024-10-08 22:53:26 +0900

    programmers

---
+++ src/main/programmers/lessons181877/Solution.java
@@ -0,0 +1,8 @@
+package main.programmers.lessons181877;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+}

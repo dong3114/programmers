fbc683573de0ed7ba53ff194f7194b5b3d866300
dong3114 <scv3114@gmail.com>
2024-11-21 00:24:23 +0900

    programmers

--- src/main/programmers/lessons181857/Main.java
+++ src/main/programmers/lessons181857/Main.java
@@ -1,8 +1,14 @@
 package main.programmers.lessons181857;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         Solution sl = new Solution();
+        int[] arr = {1,2,3,4,5,6};
+        String result = Arrays.toString(new ArrayList[]{sl.solution(arr)});
+        System.out.println(result);
 
     }
 }
--- src/main/programmers/lessons181857/Solution.java
+++ src/main/programmers/lessons181857/Solution.java
@@ -6,25 +6,27 @@ class Solution {
     public ArrayList<Integer> solution(int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
         int size = arr.length;
-        int count = 0;
-        if(check(size, count) == 1){
-            for(int i = 0; i < size; i++){
+        if(size == 0){
+            answer.add(0);
+            return answer;
+        }
+        int count = check(size);
+        for(int i = 0; i < count; i++){
+            if(i < arr.length){
                 answer.add(arr[i]);
+            } else {
+                answer.add(0);
             }
         }
-
         return answer;
     }
 
-    public int check(int size, int count){
-        if(size%2 == 0){
-            int n = size / 2;
-            int x = count + 1;
-            return check(n, x);
-        } else {
-            return size;
+    public int check(int size) {
+        int power = 1;
+        while (power < size){
+            power *= 2;
         }
+        return power;
     }
-
 }
 

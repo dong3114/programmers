7e7c0308587245f62d0df88f8bb807933a0264bf
dong3114 <scv3114@gmail.com>
2025-07-11 17:32:40 +0900

    mini-project

---
+++ src/main/burumable/land/Land.java
@@ -0,0 +1,8 @@
+package main.burumable.land;
+
+import main.burumable.user.UserInfo;
+
+// 인터페이스로 공통 메서드를 작성
+public interface Land {
+    void changeLandUser();
+}
---
+++ src/main/burumable/land/LandInfo.java
@@ -0,0 +1,9 @@
+package main.burumable.land;
+
+public class LandInfo {
+    private String landName;
+    private int landPrice;
+    private String landUserName;
+    private int landTax;
+
+}

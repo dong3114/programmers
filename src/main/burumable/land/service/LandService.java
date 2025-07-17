e4d8492e5b4603d0944f5ba1dc0fc7eb269e1ee2
dong3114 <scv3114@gmail.com>
2025-07-17 20:38:31 +0900

    mini-project

--- src/main/burumable/land/service/LandService.java
+++ src/main/burumable/land/service/LandService.java
@@ -2,5 +2,7 @@ package main.burumable.land.service;
 
 // 인터페이스로 공통 메서드를 작성
 public interface LandService {
-
+    // 1. 랜드의 자식 객체를 담아줘야함
+    // 2. setLandUser의 상태 업데이트
+    // 3. 자식 객체의 세율 올리는 로직 어디에 둘까?
 }
